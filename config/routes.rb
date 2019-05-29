ab7d0701651c0f803abd9944466c7802a64a955b
Pedro <pedromatu29@gmail.com>
2019-05-28 23:51:41 -0400

    second_commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'page/one'
+  get 'page/two'
+  root 'page#one'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
